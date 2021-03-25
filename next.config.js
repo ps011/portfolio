a8d494e51a56ff0de45f61c3a9a13e33bb6dd626
Prasheel Soni <prasheelsoni11@gmail.com>
2021-03-26 02:41:27 +0530

    feat(header): add component

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
+const path = require('path');
+
 module.exports = {
   env: {
     seoLogo: 'https://ps011.github.io/assets/images/logos/logo.png',
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
   },
 }
---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+/* eslint-disable */
+import '../styles/global.scss'
+import '../styles/argon.scss'
+// This default export is required in a new `pages/_app.js` file.
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
