fa251286db523be1a15c0c64bb7a566bd269d349
Prasheel Soni <prasheelsoni11@gmail.com>
2021-03-27 14:23:37 +0530

    refactor(header): make configurable

--- components/header/header.js
+++ components/header/header.js
@@ -1,38 +1,37 @@
-import Link from 'next/link'
-import Image from 'next/image'
 import PropTypes from 'prop-types'
 
-const Header = ({ logoUrl, navMap }) => (
-  <div className="bg-primary">
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary header-nav container">
-      <div>
-        <Link href="/">
-          <Image className="navbar-brand" height="56px" width="100px" src={logoUrl} alt="Logo" />
-        </Link>
-        <div className="collapse navbar-collapse" id="navbar-primary">
-          <div className="navbar-collapse-header">
-            <div className="row">
-              <div className="col-12 collapse-brand">
-                {/* <Link href="/">
-                <Image src={logoUrl} layout="fill" />
-              </Link> */}
+const Header = ({ logoUrl, navMap }) => {
+  console.log(navMap, navMap.length)
+  return (
+    <div className="bg-primary">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary header-nav container">
+          <a href="/">
+            <img className="navbar-brand" height="56px" src={logoUrl} alt="Logo" />
+          </a>
+          <div className="collapse navbar-collapse" id="navbar-primary">
+            <div className="navbar-collapse-header">
+              <div className="row">
+                <div className="col-12 collapse-brand">
+                  <a href="/">
+                <img src={logoUrl} />
+              </a>
+                </div>
               </div>
             </div>
+            <ul className="navbar-nav ml-lg-auto">
+              { navMap.length ? navMap.map((item) => {
+                return <li className="nav-item" key={item.href}>
+                  <a className="nav-link" href={item.href}>
+                    {item.label}
+                  </a>
+                </li>
+              }) : '' }
+            </ul>
           </div>
-          <ul className="navbar-nav ml-lg-auto">
-            {navMap.length ? navMap.map((item) => (
-              <li className="nav-item">
-                <Link className="nav-link" href={item.href}>
-                  {item.label}
-                </Link>
-              </li>
-            )) : ''}
-          </ul>
-        </div>
-      </div>
-    </nav>
-  </div>
-)
+      </nav>
+    </div>
+  )
+}
 
 Header.propTypes = {
   logoUrl: PropTypes.string.isRequired,
