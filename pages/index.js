3535e933ff1e38492573f6832e5b79511dd8c92c
Prasheel Soni <prasheelsoni11@gmail.com>
2021-03-28 01:11:29 +0530

    feat(interests): add component

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,36 @@
 import PropTypes from 'prop-types'
-import Banner from '../components/banner/banner'
+import Banner from '../sections/banner/banner'
 import Header from '../components/header/header'
 import Meta from '../components/meta/meta'
+import About from '../sections/about/about'
+import Interests from '../sections/interests/interests'
 
 export async function getStaticProps() {
-  const res = await fetch(`${process.env.BASE_URL}/site-data/${process.env.USER_ID}`)
-  const data = await res.json()
+  const siteData = await fetch(`${process.env.BASE_URL}/site-data/${process.env.USER_ID}`)
+  const data = await siteData.json()
 
+  const about = await fetch(`${process.env.BASE_URL}/about/id/${process.env.USER_ID}`)
+  const aboutData = await about.json()
   if (!data) {
     return {
       notFound: true,
     }
   }
   return {
-    props: data,
+    props: { ...data, about: aboutData },
   }
 }
 
-export default function Home({ meta, header, banner }) {
+export default function Home({
+  meta, header, banner, about,
+}) {
   return (
     <div>
       <Meta {...meta} />
       <Header {...header} />
       <Banner {...banner} />
+      <About {...about} />
+      <Interests interests={about.interests} />
       <main />
     </div>
   )
@@ -31,13 +39,15 @@ export default function Home({ meta, header, banner }) {
 Home.propTypes = {
   meta: PropTypes.object,
   header: PropTypes.object,
-  banner: PropTypes.object
+  banner: PropTypes.object,
+  about: PropTypes.object,
 }
 
 Home.defaultProps = {
   meta: {},
   header: {},
   banner: {},
+  about: {},
 }
 
 // TODO: Make about and banner attributes to be fetched from server/service
