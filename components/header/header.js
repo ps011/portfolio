263cebbfb0e45d148df600fd1f286de17c4228c3
Prasheel Soni <prasheelsoni11@gmail.com>
2021-03-27 13:49:50 +0530

    refactor(meta): make configurable

--- components/header/header.js
+++ components/header/header.js
@@ -3,33 +3,35 @@ import Image from 'next/image'
 import PropTypes from 'prop-types'
 
 const Header = ({ logoUrl, navMap }) => (
-  <nav className="navbar navbar-expand-lg navbar-dark bg-primary header-nav">
-    <div>
-      <a href="/">
-        <Image className="navbar-brand" height="48px" width="100%" src={logoUrl} alt="Logo" />
-      </a>
-      <div className="collapse navbar-collapse" id="navbar-primary">
-        <div className="navbar-collapse-header">
-          <div className="row">
-            <div className="col-12 collapse-brand">
-              {/* <Link href="/">
+  <div className="bg-primary">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary header-nav container">
+      <div>
+        <Link href="/">
+          <Image className="navbar-brand" height="56px" width="100px" src={logoUrl} alt="Logo" />
+        </Link>
+        <div className="collapse navbar-collapse" id="navbar-primary">
+          <div className="navbar-collapse-header">
+            <div className="row">
+              <div className="col-12 collapse-brand">
+                {/* <Link href="/">
                 <Image src={logoUrl} layout="fill" />
               </Link> */}
+              </div>
             </div>
           </div>
+          <ul className="navbar-nav ml-lg-auto">
+            {navMap.length ? navMap.map((item) => (
+              <li className="nav-item">
+                <Link className="nav-link" href={item.href}>
+                  {item.label}
+                </Link>
+              </li>
+            )) : ''}
+          </ul>
         </div>
-        <ul className="navbar-nav ml-lg-auto">
-          {navMap.length ? navMap.map((item) => (
-            <li className="nav-item">
-              <Link className="nav-link" href={item.href}>
-                {item.label}
-              </Link>
-            </li>
-          )) : ''}
-        </ul>
       </div>
-    </div>
-  </nav>
+    </nav>
+  </div>
 )
 
 Header.propTypes = {
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,47 @@
+import PropTypes from 'prop-types'
+import Banner from '../components/banner/banner'
 import Header from '../components/header/header'
 import Meta from '../components/meta/meta'
 
-export default function Home() {
+export async function getStaticProps() {
+  const res = await fetch('http://localhost:3001/site-data/5e7662160159fc3d220ea3e1')
+  const data = await res.json()
+  console.log(data)
+
+  if (!data) {
+    return {
+      notFound: true,
+    }
+  }
+  return {
+    props: data, // will be passed to the page component as props
+  }
+}
+
+export default function Home({ meta }) {
+  console.log('====>', meta)
   return (
     <div>
       <Meta
-        name="Prasheel Soni"
-        title="Prasheel Soni - Full Stack Engineer"
-        desc="Prasheel Soni - FullStackEngineer"
+        name={meta.name}
+        title={meta.title}
+        desc={meta.desc}
       />
       <Header logoUrl="/images/logos/logo.png" />
-      <main>
-        <h1>This is my personal website. Under development</h1>
-      </main>
+      <Banner />
+      <main />
     </div>
   )
 }
+
+Home.propTypes = {
+  meta: {
+    name: PropTypes.string.isRequired,
+  },
+}
+
+Home.defaultProps = {
+  meta: {},
+}
+
+// TODO: Make meta and header attributes to be fetched from server/service
