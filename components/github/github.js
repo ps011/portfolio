cf4f24f44500034e46d17b6b9d6ca90918ab88f0
Prasheel Soni <prasheelsoni11@gmail.com>
2021-03-29 16:14:39 +0530

    feat(animation): add animation

---
+++ components/github/github.js
@@ -0,0 +1,15 @@
+import GitHubCalendar from 'react-github-calendar'
+import PropTypes from 'prop-types'
+
+const Github = ({ username }) => (
+  <div className="text-center">
+    <h3 className="text-center my-4 display-3">My GitHub Calendar</h3>
+    <GitHubCalendar username={username} />
+  </div>
+)
+
+Github.propTypes = {
+  username: PropTypes.string.isRequired,
+}
+
+export default Github
--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,15 @@ import '../styles/argon.scss'
 import '../styles/global.scss'
 import '../styles/vendor/font-awesome/css/font-awesome.min.css'
 import '../styles/vendor/nucleo/css/nucleo.css'
-
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react'
 export default function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    AOS.init({
+      easing: "ease-out-cubic",
+      offset: 50,
+    });
+  }, []);
   return <Component {...pageProps} />
 }
