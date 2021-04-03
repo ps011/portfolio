914da33dd12ba676cf935bf1677d7adbef36b40f
Prasheel Soni <prasheelsoni11@gmail.com>
2021-04-03 14:52:21 +0530

    feat(analytics): add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -5,13 +5,25 @@ import '../styles/vendor/font-awesome/css/font-awesome.min.css'
 import '../styles/vendor/nucleo/css/nucleo.css'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import { useEffect } from 'react'
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import * as gtag from "../lib/gtag";
+const isProduction = process.env.NODE_ENV === "production";
 export default function MyApp({ Component, pageProps }) {
+  const router = useRouter();
   useEffect(() => {
     AOS.init({
       easing: "ease-out-cubic",
       offset: 50,
     });
-  }, []);
+    const handleRouteChange = (url) => {
+      /* invoke analytics function only for production */
+      if (isProduction) gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return <Component {...pageProps} />
 }
