4630fffa9fdda0016412931a928796f80d0299ca
Prasheel Soni <prasheelsoni11@gmail.com>
2021-11-18 00:44:31 +0200

    feat(photo-gallery): add package and page

---
+++ pages/photo-gallery/index.tsx
@@ -0,0 +1,54 @@
+import Gallery from "react-photo-gallery";
+import Carousel, { Modal, ModalGateway } from "react-images";
+import { useCallback, useState } from "react";
+
+export async function getStaticProps() {
+    const photosData = await fetch(`${process.env.BASE_URL}/photo-galleries/`)
+    const photos = await photosData.json()
+
+    if (!photos) {
+        return {
+            notFound: true,
+        }
+    }
+    return {
+        props: { photos },
+        revalidate: 30 * 60 * 1000,
+    }
+}
+
+const PhotoGallery = ({ photos }) => {
+    const [currentImage, setCurrentImage] = useState(0);
+    const [viewerIsOpen, setViewerIsOpen] = useState(false);
+
+    const openLightbox = useCallback((event, { photo, index }) => {
+        setCurrentImage(index);
+        setViewerIsOpen(true);
+    }, []);
+
+    const closeLightbox = () => {
+        setCurrentImage(0);
+        setViewerIsOpen(false);
+    };
+    return (
+        <div>
+            <Gallery photos={photos} onClick={openLightbox} />
+            <ModalGateway>
+                {viewerIsOpen ? (
+                    <Modal onClose={closeLightbox}>
+                        <Carousel
+                            currentIndex={currentImage}
+                            views={photos.map(x => ({
+                                ...x,
+                                srcset: x.srcSet,
+                                caption: x.title
+                            }))}
+                        />
+                    </Modal>
+                ) : null}
+            </ModalGateway>
+        </div>
+    );
+}
+
+export default PhotoGallery;
