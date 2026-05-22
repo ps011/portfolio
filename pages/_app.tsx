404ddcf967ae3f05767d94726004d2da39d2d5d5
Prasheel Soni <prasheel.soni@bolt.eu>
2026-05-22 11:54:34 +0200

    Consume shared Prasheel UI package

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,18 @@
 import "../styles/global.scss";
+import "@prasheel/ui/styles.css";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { NextIntlClientProvider } from "next-intl";
 import defaultMessages from "../messages/en.json";
 import * as gtag from "../lib/gtag";
-import { ThemeProvider } from "../lib/theme-context";
+import { ThemeProvider } from "@prasheel/ui";
 import Layout from "../components/layout/Layout";
 
 const isProduction = process.env.NODE_ENV === "production";
+const themeStorageKeys = {
+  themeId: "portfolio-theme",
+  darkMode: "portfolio-dark-mode",
+};
 
 export default function Portfolio({ Component, pageProps }) {
   const router = useRouter();
@@ -25,7 +30,11 @@ export default function Portfolio({ Component, pageProps }) {
 
   return (
     <NextIntlClientProvider locale={router.locale} messages={messages ?? defaultMessages}>
-      <ThemeProvider>
+      <ThemeProvider
+        defaultThemeId="blue"
+        defaultDarkMode={false}
+        storageKeys={themeStorageKeys}
+      >
         <Layout data={siteData} about={aboutData}>
           <Component {...restPageProps} aboutData={aboutData} />
         </Layout>
